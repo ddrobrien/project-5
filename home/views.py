eab762cf96befc67a56c7b2a6d5fca0cdeb44171
David O'Brien <ddrobrien@icloud.com>
2023-01-17 11:01:42 +0000

    login.html and register.html added and linked in views.py

--- home/views.py
+++ home/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def login_page(request):
+    return render(request, 'login-html')
+
+
+def register_page(request):
+    return render(request, 'register.html')
