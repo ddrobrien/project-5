4668d2b8480f4bd8e2efdb23c7f40c4869711557
David O'Brien <ddrobrien@icloud.com>
2023-01-17 13:45:44 +0000

    linked up login and regsitration views and models

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 
 def home(request):
@@ -8,6 +10,25 @@ def home(request):
 
 
 def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user_obj = User.objects.filter(username = username)
+
+        if not user_obj.exists():
+            messages.warning(request, 'Account not found')
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+        user_obj = authenticate(username = username, password = password)
+        if not user_obj:
+            messages.warning(request, 'Invalid password')
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+        login(request, user_obj)
+        return redirect('/')
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     return render(request, 'login.html')
 
 
@@ -20,5 +41,11 @@ def register_page(request):
 
         if user_obj.exists():
             messages.warning(request, 'Username already exists')
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-    return render(request, 'register.html')
+        user = User.objects.create(username = username)
+        user.set_password(password)
+        user.save()
+        return redirect('/')
+
+    return render(request , 'register.html')
