4cc4b383f8e1b725cf1b3ae7c22c7a85563a03cb
David O'Brien <ddrobrien@icloud.com>
2023-01-17 11:47:06 +0000

    linking up the views.py for regsiter and login pages

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def home(request):
@@ -6,8 +8,17 @@ def home(request):
 
 
 def login_page(request):
-    return render(request, 'login-html')
+    return render(request, 'login.html')
 
 
 def register_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user_obj = User.objects.filter(username = username)
+
+        if user_obj.exists():
+            messages.warning(request, 'Username already exists')
+
     return render(request, 'register.html')
